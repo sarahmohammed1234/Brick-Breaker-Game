3bc895fc0216295e75e0ec99c5d75472c74fca34
sarahmohammed1234 <94794262+sarahmohammed1234@users.noreply.github.com>
2021-12-04 18:35:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
-import java.awt.Color;
 
+package BrickBreaker;
 import javax.swing.JFrame;
 
 public class Main {
-	public static void main(String[] args) {
-		JFrame obj=new JFrame();
-		Gameplay gamePlay = new Gameplay();
-		
-		obj.setBounds(10, 10, 700, 600);
-		obj.setTitle("Breakout Ball");		
-		obj.setResizable(false);
-		obj.setVisible(true);
-		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		obj.add(gamePlay);
-                obj.setVisible(true);
-		
-	}
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds (10,10,700,600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+
+    
 
 }
